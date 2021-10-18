17cf8b4d81209325c6f5660ca946b512dc279196
japheth198 <japhethjuur@gmail.com>
2021-10-18 12:57:07 +0300

    Reg 1 - numbers 1-100 - pass

--- src/ee/tthk/tarpe19/juur/FizzBuzz.java
+++ src/ee/tthk/tarpe19/juur/FizzBuzz.java
@@ -2,9 +2,15 @@ package ee.tthk.tarpe19.juur;
 
 public class FizzBuzz {
 
+	private int[] numbers;
 	public int[] getNumbers() {
-		return new int[100];
+		numbers = new int[100];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i]=i+1;
+		}
+		return numbers;
 	}
+	
 
 	public Object convert(int i) {
 		if (i % 3 == 0) {
