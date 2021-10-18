3d76291338d794fc41300c004c5939acb65ef285
japheth198 <japhethjuur@gmail.com>
2021-10-18 20:01:05 +0300

    Req 4 - FizzBuzz = /3 & /5 - pass

--- src/ee/tthk/tarpe19/juur/FizzBuzz.java
+++ src/ee/tthk/tarpe19/juur/FizzBuzz.java
@@ -16,8 +16,8 @@ public class FizzBuzz {
 	}
 	
 	private String getFizzBuzz(int number) {
-		/*if (number%5==0 && number%3==0) 
-			return "FizzBuzz";*/
+		if (number%5==0 && number%3==0) 
+			return "FizzBuzz";
 		if (number%5==0) 
 			return "Buzz";
 		if (number%3==0) 
