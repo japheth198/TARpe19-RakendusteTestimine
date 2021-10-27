45595039a29c3d895633019a7949e374e0f30cbe
japheth198 <japhethjuur@gmail.com>
2021-10-27 14:06:56 +0300

    Req3 - assert ip has 3 numbers - fail

--- src/ee/tthk/tarpe19/juur/ipValidatorTests.java
+++ src/ee/tthk/tarpe19/juur/ipValidatorTests.java
@@ -28,4 +28,10 @@ class ipValidatorTests {
 		assertTrue(validator.ValidateIpv4Address("1.2.3.1"));
 	}
 
+	@Test
+	void shouldReturnFourNumbersGivenStringWithThreeDots() {
+		IpValidator validator = new IpValidator();
+		int[] expected = {1,2,3,4};
+		assertArrayEquals(expected,validator.getNumbers("1.2.3.4"));
+	}
 }
