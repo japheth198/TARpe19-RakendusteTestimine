cbe09db6db8ab6b39b313e8ff58fe2e6bcf41cb5
japheth198 <japhethjuur@gmail.com>
2021-10-27 13:50:11 +0300

    Req2 - assert string has 3 . - pass

--- src/ee/tthk/tarpe19/juur/IpValidator.java
+++ src/ee/tthk/tarpe19/juur/IpValidator.java
@@ -1,14 +1,17 @@
 package ee.tthk.tarpe19.juur;
 
-import java.util.function.BooleanSupplier;
 
 public class IpValidator {
 
 	public Boolean ValidateIpv4Address(String ipString) {
-		if(ipString=="") {
-			return false;
+		if(hasThreeDots(ipString)) {
+			return true;
 		}
 		return false;
+	}
+
+	private boolean hasThreeDots(String ipString) {
+		return ipString.chars().filter(c->c=='.').count()==3;
 	}
 
 }
