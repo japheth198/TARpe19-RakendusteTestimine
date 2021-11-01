ffb392b917e69c754793479cd2b9bdf2b4f170a0
japheth198 <japhethjuur@gmail.com>
2021-11-01 09:53:13 +0200

    Req6 : pass

--- src/ee/tthk/tarpe19/juur/IpValidator.java
+++ src/ee/tthk/tarpe19/juur/IpValidator.java
@@ -7,10 +7,16 @@ public class IpValidator {
 	public Boolean ValidateIpv4Address(String ipString) {
 		if(hasThreeDots(ipString) && 
 			hasFourNumbers(ipString) &&
-			numbersInRange0_255(ipString)){
+			numbersInRange0_255(ipString) &&
+			firstNumberInRange1_254(ipString)){
 			return true;
 		}
 		return false;
+	}
+
+	private boolean firstNumberInRange1_254(String ipString) {
+		return getNumbers(ipString)[0]>0 &&
+				getNumbers(ipString)[0]<255;
 	}
 
 	private boolean numbersInRange0_255(String ipString) {
